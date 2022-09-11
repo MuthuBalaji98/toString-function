301580e47b20787bab17bc736a7554f7713e9e33
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:22:21 +0530

    toString

---
+++ main.js
@@ -0,0 +1,7 @@
+let text="Java Script";
+console.log("original array :",text);
+console.log("length of the array is "+text.length);
+console.log("method : array.toString()");
+let result=text.toString();
+console.log("change in array : ",result);
+console.log("return: length of array after toString() is "+result.length);
